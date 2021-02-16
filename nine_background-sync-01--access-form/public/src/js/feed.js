365b25a5c155dc030a6484ef9f1aa1e675b36c60
sirkusr <sosial.b96@gmail.com>
2021-02-16 08:38:22 +0700

    Background Sync

---
+++ nine_background-sync-01--access-form/public/src/js/feed.js
@@ -0,0 +1,192 @@
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
+var sharedMomentsArea = document.querySelector("#shared-moments");
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title");
+var locationInput = document.querySelector("#location");
+
+function openCreatePostModal() {
+  // createPostArea.style.display = 'block';
+  // setTimeout(function() {
+  createPostArea.style.transform = "translateY(0)";
+  // }, 1);
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
+      } else {
+        console.log("User added to home screen");
+      }
+    });
+
+    deferredPrompt = null;
+  }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function(registrations) {
+  //       for (var i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.transform = "translateY(100vh)";
+  // createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener("click", openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log("clicked");
+  if ("caches" in window) {
+    caches.open("user-requested").then(function (cache) {
+      cache.add("https://httpbin.org/get");
+      cache.add("/src/images/sf-boat.jpg");
+    });
+  }
+}
+
+function clearCards() {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
+
+function createCard(data) {
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  var cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = "url(" + data.image + ")";
+  cardTitle.style.backgroundSize = "cover";
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.style.color = "white";
+  cardTitleTextElement.className = "mdl-card__title-text";
+  cardTitleTextElement.textContent = data.title;
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = data.location;
+  cardSupportingText.style.textAlign = "center";
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+function updateUI(data) {
+  clearCards();
+  for (var i = 0; i < data.length; i++) {
+    createCard(data[i]);
+  }
+}
+
+var url = "https://pwagram-bf1ef-default-rtdb.firebaseio.com/posts.json";
+var networkDataReceived = false;
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    networkDataReceived = true;
+    console.log("From web", data);
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
+    }
+    updateUI(dataArray);
+  });
+
+if ("indexedDB" in window) {
+  readAllData("posts").then(function (data) {
+    if (!networkDataReceived) {
+      console.log("From cache", data);
+      updateUI(data);
+    }
+  });
+}
+
+function sendData() {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json.stringify({
+      id: new Date().toISOString(),
+      title: titleInput.value,
+      location: locationInput.value,
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/pwagram-bf1ef.appspot.com/o/sf-boat.jpg?alt=media&token=76857877-a737-42b4-a0ba-6d3b94afea95",
+    }),
+  }).then((res) => {
+    console.log("Send data", res);
+    updateUI();
+  });
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (titleInput.value.trim() === "" || locationInput.value.trim() === "") {
+    alert("Please enter valid data!");
+    return;
+  }
+
+  closeCreatePostModal();
+
+  // sync manager is basically the API through which we use the background synchronization features and it's important
+  // to understand how browsers support looks like for that feature.
+
+  // do at here because service worker cant listen dom
+  if ("serviceWorker" in navigator && "SyncManager" in window) {
+    // First of all, I will reach out to my service worker and there I can call the ready property to make sure
+    // that it has been configured, that it is installed and activated, that it is ready to take some input.
+    navigator.serviceWorker.ready.then((sw) => {
+      var post = {
+        id: new Date().toISOString(),
+        title: titleInput.value,
+        location: locationInput.value,
+      };
+      writeData("sync-posts", post)
+        .then(() => {
+          // We do this by using the service worker and calling sync,
+          // this gives us access to
+          // the sync manager now from the service workers point of view and there we can call the register method.
+          // The register method just as you would assume allows us to register a synchronization task with the service worker.
+          // It takes only one input and that is an ID, a tag
+          // we can add or we can use to clearly identify a given synchronization task.
+          return sw.sync.register("sync-new-post");
+        })
+        .then(() => {
+          var snackBarContainer = document.querySelector("#confirmation-toast");
+          var data = { message: "Your Post was saved for syncing!" };
+          snackBarContainer.MaterialSnackbar.showSnackbar(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  } else {
+    sendData();
+  }
+});
